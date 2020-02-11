d8951f45457bcad71f2737f4de0fd207d8c4d73e
fbzr <fabricio.spb@gmail.com>
2020-02-11 12:30:27 -0800

    change color and size of nav when scroll

--- js/index.js
+++ js/index.js
@@ -4,3 +4,17 @@
 window.addEventListener('load', () => {
     alert('Welcome to Fun Bus!!');
 });
+
+// Add event listner to change nav background color when scroll
+const changeNavColor = e => {    
+    const nav = document.querySelector('header.main-navigation');
+    const navHeight = 90; // parseInt(nav.style.height);
+
+    if ((document.body.scrollTop >= navHeight || document.documentElement.scrollTop >= navHeight)) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+}
+
+window.addEventListener('scroll', changeNavColor);
