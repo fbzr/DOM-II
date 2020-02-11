8a996110c20524687b1a08410fc356916dcb267d
fbzr <fabricio.spb@gmail.com>
2020-02-11 13:45:19 -0800

    add more event listeners

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ window.addEventListener('load', () => {
 // Add event listner to change nav background color when scroll
 const changeNavColor = e => {    
     const nav = document.querySelector('header.main-navigation');
-    const navHeight = 90; // parseInt(nav.style.height);
+    const navHeight = 90;
 
     if ((document.body.scrollTop >= navHeight || document.documentElement.scrollTop >= navHeight)) {
         nav.classList.add('scrolled');
@@ -32,8 +32,43 @@ document.querySelectorAll('.nav-link').forEach(link => {
     });    
 });
 
-// Disable right click
+// Disable context menu
 document.body.addEventListener('contextmenu', e => {
     e.preventDefault();   
 });
 
+// Change background color on mouse move
+const changeColorOnMouseMove = (e) => {
+    document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 0`;
+}
+
+// Start crazy colors on background when auxclick on logo
+document.querySelector('.logo-heading').addEventListener('auxclick', () => {
+    document.body.addEventListener('mousemove', changeColorOnMouseMove);
+});
+
+// Stop the nav items from refreshing the page
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', e => e.preventDefault());
+});
+
+// Add mouse over event listener to all content-section 
+document.querySelectorAll('section.content-section').forEach(section => {
+    console.log(section);
+    section.addEventListener('mouseenter', e => section.classList.add('mouseOver'));
+    section.addEventListener('mouseleave', e => section.classList.remove('mouseOver'));
+});
+
+// Add animation on Content destination img when use wheel
+document.querySelector('.content-destination img').addEventListener('wheel', e => {
+    e.preventDefault();
+    
+    e.target.animate([
+            { transform: 'rotateX(0)' }, 
+            { transform: 'rotateX(360deg)' }
+          ], { 
+            duration: 500,
+            iterations: 1
+          });
+    console.log(e.target);
+});
